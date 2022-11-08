f94455b7fa41770190f1fa9c2379459a7cbc2b2f
Jimmy-Mc-Gill <114485586+Jimmy-Mc-Gill@users.noreply.github.com>
2022-11-08 13:43:29 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup(){
+    console.log("setup");
+    
+    createCanvas(300,300);
+
+}
+
+
+function draw(){
+
+    console.log("draw");
+    
+      var red=random(0,255)
+       var green=random(0,255)
+       var blue=random(0,255)
+       background(red,green,blue);
+       rect(150,150,80,80);
+}
